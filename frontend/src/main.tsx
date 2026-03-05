dd1beeb241a3614a91ec4411c11e197e2ce9f09b
Lucas Kluska Donini <lkd.lucas@gmail.com>
2026-03-05 14:59:16 -0300

    feat(professor): add Observations page for teachers

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,6 +7,7 @@ import Home from './pages/Home'
 import Login from './pages/Login/Login'
 import ProfessorHome from './pages/ProfessorHome/ProfessorHome'
 import TelaNotasProfessor from './pages/TelaNotasProfessor/TelaNotasProfessor'
+import TelaObservacoesProfessor from './pages/TelaObservacoesProfessor/TelaObservacoesProfessor'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -15,7 +16,8 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/professor" element={<ProfessorHome />} />
-        <Route path="/professor/nota-aluno" element={<TelaNotasProfessor />} />
+        <Route path="/professor/aluno/:matricula/notas" element={<TelaNotasProfessor />} />
+        <Route path="/professor/aluno/:matricula/observacoes" element={<TelaObservacoesProfessor />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>

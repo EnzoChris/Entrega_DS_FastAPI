1f85e3042b7fbc669a7287a6f224b22d1ad1d165
Lucas Kluska Donini <lkd.lucas@gmail.com>
2026-03-05 14:40:55 -0300

    feat(login): add login page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-import Home from './pages/Home.tsx'
+
+import Home from './pages/Home'
+import Login from './pages/Login/Login'
+import ProfessorHome from './pages/ProfessorHome/ProfessorHome'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Home />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/professor" element={<ProfessorHome />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
